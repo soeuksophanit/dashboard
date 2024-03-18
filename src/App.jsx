66d1430c00dc13c46900e24c01848415ed424338
sophanit <sophanit@sophanits-MacBook-Pro.local>
2024-03-18 20:29:44 +0700

    remove usestate from app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import Container from "./components/Container";
 import ProjectBoard from "./components/ProjectBoard";
 import NavBar from "./components/NavBar";
